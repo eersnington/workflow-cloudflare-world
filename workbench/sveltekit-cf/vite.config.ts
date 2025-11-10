76f71f7303661783e58376d2c8fc40fde49aec42
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-10 22:06:06 +0400

    Update vite.config.ts

--- workbench/sveltekit-cf/vite.config.ts
+++ workbench/sveltekit-cf/vite.config.ts
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { workflowPlugin } from 'workflow/sveltekit';
-import { cloudflareWorkflowTransformer } from 'workflow-cloudflare-world';
+import { cloudflareWorkflowTransformer } from 'workflow-cloudflare-world/vite-plugin';
 
 export default defineConfig({
   plugins: [
