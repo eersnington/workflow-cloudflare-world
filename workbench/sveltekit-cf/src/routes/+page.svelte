b479896a7db01ebcbc9b2bd72ede75cb85db6755
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-10 11:24:21 +0400

    Update +page.svelte

--- workbench/sveltekit-cf/src/routes/+page.svelte
+++ workbench/sveltekit-cf/src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1>Workflows DevKit + SvelteKit Example</h1>
+<h1>Workflows DevKit + Cloudflare World + SvelteKit Example</h1>
 <hr />
 <textarea id="output" readonly placeholder="output"></textarea>
