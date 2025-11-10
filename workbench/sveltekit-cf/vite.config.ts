a77db01a41e20cbd0fd3a7d424ff34dea4dc4914
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-10 21:33:28 +0400

    Update vite.config.ts

--- workbench/sveltekit-cf/vite.config.ts
+++ workbench/sveltekit-cf/vite.config.ts
@@ -2,12 +2,29 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { workflowPlugin } from 'workflow/sveltekit';
+import { cloudflareWorkflowTransformer } from 'workflow-cloudflare-world';
 
 export default defineConfig({
-  plugins: [workflowPlugin(), devtoolsJson(), sveltekit()],
+  plugins: [
+    workflowPlugin(),
+    cloudflareWorkflowTransformer(),
+    devtoolsJson(),
+    sveltekit(),
+  ],
+  ssr: {
+    external: [
+      'workflow-cloudflare-world',
+      '@workflow/world',
+      '@workflow/core',
+    ],
+  },
   build: {
     rollupOptions: {
-      external: ['cloudflare:workers'],
+      external: [
+        'cloudflare:workers',
+        'workflow-cloudflare-world',
+        '@workflow/world',
+      ],
     },
   },
 });
