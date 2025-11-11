221bcf9eed6544914c9212711f6fddf8115acae0
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-11 13:33:24 +0400

    removed old

--- workbench/sveltekit/src/app.d.ts
+++ workbench/sveltekit/src/app.d.ts
@@ -6,9 +6,7 @@ declare global {
     // interface Locals {}
     // interface PageData {}
     // interface PageState {}
-    interface Platform {
-      env: import('workflow-cloudflare-world').CloudflareEnv;
-    }
+    // interface Platform {}
   }
 }
 
--- workbench/sveltekit/vite.config.ts
+++ workbench/sveltekit/vite.config.ts
@@ -2,13 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { workflowPlugin } from 'workflow/sveltekit';
-import { cloudflareWorkflowTransformer } from 'workflow-cloudflare-world/vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    workflowPlugin(),
-    cloudflareWorkflowTransformer(),
-    devtoolsJson(),
-    sveltekit(),
-  ],
+  plugins: [workflowPlugin(), devtoolsJson(), sveltekit()],
 });
