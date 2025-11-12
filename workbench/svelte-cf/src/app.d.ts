ad77f28f8a654fdce88347bf05cf628a132e65c5
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-12 13:05:25 +0400

    Update app.d.ts

--- workbench/svelte-cf/src/app.d.ts
+++ workbench/svelte-cf/src/app.d.ts
@@ -1,12 +1,16 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
+import type {
+  DurableObjectNamespace,
+  KVNamespace,
+} from '@cloudflare/workers-types';
+
 declare global {
   namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
-    // interface PageState {}
-    // interface Platform {}
+    interface Platform {
+      env?: {
+        YOUR_KV_NAMESPACE: KVNamespace;
+        YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace;
+      };
+    }
   }
 }
 
