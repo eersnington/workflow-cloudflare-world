aa7de8b913125b1d90ba0b35443a0d22650092ee
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-18 22:29:26 +0400

    Update init.ts

--- packages/workflow-studio-cli/src/commands/init.ts
+++ packages/workflow-studio-cli/src/commands/init.ts
@@ -490,7 +490,7 @@ async function scaffoldWithFrameworkCli({
   }
 
   if (template === 'hono') {
-    const cliArgs = [projectSpecifier, '--', '--template=nodejs'];
+    const cliArgs = [projectSpecifier, '--template=nodejs'];
     await withWorkspaceFileHidden(cwd, () =>
       runWithPackageManagerExecutor({
         packageManager,
