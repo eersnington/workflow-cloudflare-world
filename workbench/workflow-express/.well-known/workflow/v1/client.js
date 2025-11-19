0593661897c1f91313249bb18e0401862f9c7388
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-19 22:57:05 +0400

    unignoring client.js and typed export

---
+++ workbench/workflow-express/.well-known/workflow/v1/client.js
@@ -0,0 +1,13 @@
+// biome-ignore-all lint: generated file
+/* eslint-disable */
+
+
+// workflows/example.ts
+async function handleGreeting(name) {
+  throw new Error("You attempted to execute workflow handleGreeting function directly. To start a workflow, use start(handleGreeting) from workflow/api");
+}
+handleGreeting.workflowId = "workflow//workflows/example.ts//handleGreeting";
+export {
+  handleGreeting
+};
+//# sourceMappingURL=client.js.map
