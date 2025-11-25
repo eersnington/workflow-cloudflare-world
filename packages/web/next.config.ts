b2c6b1f44721132376124403848ce8775d49a70f
Sree Narayanan <sreeaadhi07@gmail.com>
2025-11-25 05:15:30 +0400

    added react compiler

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   productionBrowserSourceMaps: false,
   experimental: {
     serverSourceMaps: false,
